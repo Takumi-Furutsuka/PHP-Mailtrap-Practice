c01c6d93955fdc02180ef3c0af82574ba66e5683
Takumi Furutsuka <takumihojo@gmail.com>
2021-09-13 08:18:18 +0900

    PHP、Mailtrapの練習用

---
+++ Encode.php
@@ -0,0 +1,9 @@
+<?php
+function es($str, $charset = 'UTF-8') {
+  return htmlspecialchars($str, ENT_QUOTES, $charset);
+}
+
+function format($datetime, $format = 'yyyy/MM/dd') {
+  $ts = strtotime($datetime);
+  print(date($format, $ts));
+}
